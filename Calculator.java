88d2924f73255d8e947b3a3e5a9d88f79a9bbee0
Shal <shal2005@list.ru>
2023-04-12 22:51:42 +0300

    add files

---
+++ Calculator.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        if (y != 0) {
+            return x / y;
+        } else {
+            System.out.println("На ноль делить нельзя");
+            return Integer.MAX_VALUE;
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Object> println = System.out::println;
+}
